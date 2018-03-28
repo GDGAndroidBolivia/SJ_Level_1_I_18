838696e2e0c71b5d19e21505c495f434c836d776
Marcelo Torrez <elmac395@gmail.com>
2018-03-27 22:04:21 -0400

    Repositorio de la tercera clase creado

---
+++ SJ_L1_03_ViewGroups/app/src/main/java/com/example/marcelo/sj_l1_03_viewgroups/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.marcelo.sj_l1_03_viewgroups;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
