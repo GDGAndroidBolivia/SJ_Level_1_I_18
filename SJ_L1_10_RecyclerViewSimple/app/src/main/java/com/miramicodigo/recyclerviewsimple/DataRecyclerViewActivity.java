12bca9566beecd1345c7bcfe200b7181148ea520
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-04-24 01:41:59 -0400

    Demo RecyclerView simple

--- SJ_L1_10_RecyclerViewSimple/app/src/main/java/com/miramicodigo/recyclerviewsimple/DataRecyclerViewActivity.java
+++ SJ_L1_10_RecyclerViewSimple/app/src/main/java/com/miramicodigo/recyclerviewsimple/DataRecyclerViewActivity.java
@@ -23,8 +23,14 @@ public class DataRecyclerViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_recycler_view);
 
+        recyclerView = (RecyclerView) findViewById(R.id.rvDatos);
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(getApplicationContext()));
 
+        listaDatos = llenarDatos();
 
+        adaptador = new RecyclerViewAdapter(listaDatos);
+        recyclerView.setAdapter(adaptador);
     }
 
     public List<String> llenarDatos() {
@@ -36,44 +42,39 @@ public class DataRecyclerViewActivity extends AppCompatActivity {
     }
 
     private class RecyclerViewHolder extends RecyclerView.ViewHolder {
-
-
+        private TextView tvTexto;
 
         public RecyclerViewHolder(View itemView) {
             super(itemView);
-
-
-
+            tvTexto = (TextView) itemView;
         }
     }
 
     private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
-
-
+        private List<String> data;
 
         public RecyclerViewAdapter(List<String> a) {
-
-
-
+            data = a;
         }
 
         @Override
         public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-
-            return null;
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1,
+                    parent, false);
+            return new RecyclerViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(RecyclerViewHolder holder, int position) {
-
-
-
+            String dato = data.get(position);
+            holder.tvTexto.setText(dato);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return data.size();
         }
     }
+
 }
