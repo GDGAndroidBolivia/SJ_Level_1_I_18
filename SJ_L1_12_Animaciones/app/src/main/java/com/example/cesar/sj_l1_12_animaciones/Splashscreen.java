74c5af7f12403bea64f46b346a6578fa02ba1a8c
Julio Cesar Ticona <cesarju.cc@gmail.com>
2018-04-25 19:38:26 -0400

    Animaciones

---
+++ SJ_L1_12_Animaciones/app/src/main/java/com/example/cesar/sj_l1_12_animaciones/Splashscreen.java
@@ -0,0 +1,13 @@
+package com.example.cesar.sj_l1_12_animaciones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
