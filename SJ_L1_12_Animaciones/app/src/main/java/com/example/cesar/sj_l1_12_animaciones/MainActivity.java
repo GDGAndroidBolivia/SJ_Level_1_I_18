1ea9273245fc80a0899dfa0853f84c41b481b232
Julio Cesar Ticona <cesarju.cc@gmail.com>
2018-04-30 21:14:59 -0400

    cambios en el gradel

--- SJ_L1_12_Animaciones/app/src/main/java/com/example/cesar/sj_l1_12_animaciones/MainActivity.java
+++ SJ_L1_12_Animaciones/app/src/main/java/com/example/cesar/sj_l1_12_animaciones/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.cesar.sj_l1_12_animaciones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imag1;
+
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imag1 = (ImageView)findViewById(R.id.iron_man);
+
+    }
+
+
+    public void findin(View view){
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        imag1.startAnimation(animation);
+    }
+    public void findout(View view){
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+        imag1.startAnimation(animation);
+    }
+    public void zoomin(View view){
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        imag1.startAnimation(animation);
     }
 }
